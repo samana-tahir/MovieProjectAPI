9410e9e829af8a8eab311c38f94f42ae119f43af
mi-app <app@massiveinfinity.com>
2018-08-20 12:21:34 +0800

    Initial Commit

---
+++ MovieProjectAPI/MovieProjectAPI.h
@@ -0,0 +1,19 @@
+//
+//  MovieProjectAPI.h
+//  MovieProjectAPI
+//
+//  Created by Samana Tahir on 20/08/2018.
+//  Copyright © 2018 Samana Tahir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MovieProjectAPI.
+FOUNDATION_EXPORT double MovieProjectAPIVersionNumber;
+
+//! Project version string for MovieProjectAPI.
+FOUNDATION_EXPORT const unsigned char MovieProjectAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieProjectAPI/PublicHeader.h>
+
+
